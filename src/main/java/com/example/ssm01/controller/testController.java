d12d663be62132d1653d2531ccbc7f852e3f0e95
BetterLeezm <leezhaomin@163.com>
2023-07-24 10:07:16 +0800

    测试的修改，24号——11

--- src/main/java/com/example/ssm01/controller/testController.java
+++ src/main/java/com/example/ssm01/controller/testController.java
@@ -12,9 +12,6 @@ public class testController {
         return "hi！！！";
         //这里添加一行注释 pull下来
         //之后进行添加
-        //新的内容进行添加 tylerlli 进行修改
-        //添加新的修改
-        //更换用户添加新的修改
-
+        //24号 tylerlli修改1
     }
 }
