3f7d70422e263e40ad6f39a65acda1dcdd340a40
tylerlli <tylerlli@tencent.com>
2023-07-24 09:49:41 +0800

    测试的修改，24号

--- src/main/java/com/example/ssm01/controller/testController.java
+++ src/main/java/com/example/ssm01/controller/testController.java
@@ -1,7 +1,5 @@
 package com.example.ssm01.controller;
 
-import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,5 +12,7 @@ public class testController {
         return "hi！！！";
         //这里添加一行注释 pull下来
         //之后进行添加
+        //新的内容进行添加 tylerlli 进行修改
+        //添加新的修改
     }
 }
